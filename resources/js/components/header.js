f7aac370e2d6a8a294aaad31510abaad96df4c22
Unknown <nagyervin.bws@gmail.com>
2018-12-10 20:31:03 +0200

    Added rudimentary menu

--- resources/js/components/header.js
+++ resources/js/components/header.js
@@ -5,6 +5,17 @@ const Header = () => (
     <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
         <div className='container'>
             <Link className='navbar-brand' to="/app">Intranet</Link>
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'
+>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <Link className='nav-link' to="/articles">Articles</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link className='nav-link' to="/articles/add">Add article</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 )
